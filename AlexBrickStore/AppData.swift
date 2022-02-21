d4c92bf21d23c930cf862e9200ebd6cf4e919207
Alexandre Bintz <alexandre@bintz.io>
2022-02-21 17:01:13 +0100

    list orders

--- AlexBrickStore/AppData.swift
+++ AlexBrickStore/AppData.swift
@@ -4,5 +4,14 @@ import Foundation
 
 
 struct AppData: Codable {
+
     
+    var orders: [Order] = []
+}
+
+
+
+struct Order: Codable, Identifiable {
+    
+    let id: String
 }
--- AlexBrickStore/ContentView.swift
+++ AlexBrickStore/ContentView.swift
@@ -5,10 +5,25 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    @EnvironmentObject var appDataStorage: AppDataStorage
+    
+    
     var body: some View {
         
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        let orders = appDataStorage.appData?.orders ?? []
+        if orders.isEmpty {
+            
+            Text("No orders")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+        } else {
+        
+            ForEach(appDataStorage.appData?.orders ?? []) { order in
+            
+                Text(order.id)
+            }
+        }
     }
 }
 
