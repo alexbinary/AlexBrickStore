863d91abb70c2a0b45e021dffca8581f8c8d9c02
Alexandre Bintz <alexandre@bintz.io>
2022-02-22 17:40:24 +0100

    compute stats between dates

--- AlexBrickStore/StatsView.swift
+++ AlexBrickStore/StatsView.swift
@@ -10,17 +10,25 @@ struct StatsView: View {
     
     @EnvironmentObject var appDataStorage: AppDataStorage
     
+    @State var dateStart: Date = Date()
+    @State var dateEnd: Date = Date()
+    
     
     var body: some View {
         
         VStack {
+            
+            DatePicker("Start", selection: self.$dateStart)
+            DatePicker("End", selection: self.$dateEnd)
     
-            let shippingBilledValues = self.appDataStorage.appData.orders.compactMap { Double($0.shippingBilled) }
+            let orders = self.appDataStorage.appData.orders.filter { $0.orderDate >= self.dateStart && $0.orderDate <= self.dateEnd }
+            
+            let shippingBilledValues = orders.compactMap { Double($0.shippingBilled) }
             let averageShippingBilled = shippingBilledValues.reduce(0.0, +) / Double(shippingBilledValues.count)
             
             Text("Average shipping billed: \(averageShippingBilled)")
             
-            let shippingMyCostValues = self.appDataStorage.appData.orders.compactMap { Double($0.shippingMyCost) }
+            let shippingMyCostValues = orders.compactMap { Double($0.shippingMyCost) }
             let averageShippingMyCost = shippingMyCostValues.reduce(0.0, +) / Double(shippingMyCostValues.count)
             
             Text("Average shipping my cost: \(averageShippingMyCost)")
