30ae77694c445c34ccb273c8e40bead857532aa6
Alexandre Bintz <alexandre@bintz.io>
2022-02-23 16:17:36 +0100

    load test data

--- AlexBrickStore/CloudStorage/CloudStorageAgent.swift
+++ AlexBrickStore/CloudStorage/CloudStorageAgent.swift
@@ -8,5 +8,12 @@ class CloudStorageAgent {
     
     func start() {
         
+        let testDataUrl = Bundle.main.url(forResource: "bricklinktestdata", withExtension: "json")!
+        print("Loading test data from \(testDataUrl)")
+        
+        let testDataRaw = try! Data(contentsOf: testDataUrl)
+        let testDataDecoded: BrickLinkAPIResponse<[BrickLinkOrder]> = testDataRaw.decode()
+        
+        
     }
 }
