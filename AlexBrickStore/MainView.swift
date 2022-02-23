7f0f02ea6a8831712169e4bccfb35e86533aaa2c
Alexandre Bintz <alexandre@bintz.io>
2022-02-23 13:49:15 +0100

    tabs for orders/stats

--- AlexBrickStore/MainView.swift
+++ AlexBrickStore/MainView.swift
@@ -9,61 +9,62 @@ struct MainView: View {
     @EnvironmentObject var appDataStorage: AppDataStorage
     
     @State var selectedOrderId: UUID? = nil
-    @State var sheetPresented = false
     
     
     var body: some View {
         
-        NavigationView {
+        TabView {
+        
+            NavigationView {
 
-            VStack {
+                VStack {
 
-                Button(action: {
-                    self.sheetPresented = true
-                }, label: {
-                    Text("Stats")
-                })
+                    NavigationLink("Add order", destination: OrderView(order: Order(brickLinkId: "", orderDate: Date(), totalItems: "", shippingBilled: "", shippingMyCost: "")))
+                    
+                    let orders = appDataStorage.appData?.orders ?? []
+                    if orders.isEmpty {
 
-                NavigationLink("Add order", destination: OrderView(order: Order(brickLinkId: "", orderDate: Date(), totalItems: "", shippingBilled: "", shippingMyCost: "")))
-                
-                let orders = appDataStorage.appData?.orders ?? []
-                if orders.isEmpty {
+                        Text("No orders")
 
-                    Text("No orders")
+                    } else {
 
-                } else {
+                        List(orders) { order in
 
-                    List(orders) { order in
-
-                        NavigationLink(
-                            destination: OrderView(order: order),
-                            tag: order.internalId,
-                            selection: self.$selectedOrderId
-                        ) {
-                            HStack {
-                                Text(order.brickLinkId)
-                                Spacer()
-                                Button {
-                                    self.appDataStorage.deleteOrder(order)
-                                    self.selectedOrderId = (appDataStorage.appData?.orders ?? []).first?.internalId
-                                } label: {
-                                    Text("Delete")
+                            NavigationLink(
+                                destination: OrderView(order: order),
+                                tag: order.internalId,
+                                selection: self.$selectedOrderId
+                            ) {
+                                HStack {
+                                    Text(order.brickLinkId)
+                                    Spacer()
+                                    Button {
+                                        self.appDataStorage.deleteOrder(order)
+                                        self.selectedOrderId = (appDataStorage.appData?.orders ?? []).first?.internalId
+                                    } label: {
+                                        Text("Delete")
+                                    }
                                 }
                             }
                         }
+                        .frame(minHeight: 500)
                     }
-                    .frame(minHeight: 500)
                 }
-            }
-            .frame(width: 300)
+                .frame(width: 300)
 
-            Text("No order selected")
-                .frame(maxWidth: .infinity)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .sheet(isPresented: self.$sheetPresented) {
+                Text("No order selected")
+                    .frame(maxWidth: .infinity)
+            }
+                .tabItem {
+                    Text("Orders")
+                }
+            
             StatsView()
                 .environmentObject(self.appDataStorage)
+                .tabItem {
+                    Text("Stats")
+                }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
